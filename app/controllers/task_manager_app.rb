3dc41f219eeb1e432e4a6882c2c8f66d39cbf85e
Brandon Stewart <boveus@gmail.com>
2017-08-09 20:06:36 -0600

    initial commit

---
+++ app/controllers/task_manager_app.rb
@@ -0,0 +1,32 @@
+#bookmark
+#https://github.com/turingschool-examples/task_manager_redux#linking-our-app-to-our-database
+
+
+require_relative '../models/task.rb'
+
+class TaskManagerApp < Sinatra::Base
+  set :root, File.expand_path("..", __dir__)
+
+  get '/' do
+    erb :dashboard
+  end
+
+  # get '/tasks' do
+  # @tasks = ["task1", "task2", "task3"]
+  # erb :index
+  # end
+
+  post '/tasks' do
+    task = Task.new(params[:task])
+    task.save
+    redirect '/tasks'
+  end
+
+  get '/tasks/new' do
+    erb :new
+  end
+
+  post '/tasks' do
+  "<p>Params: #{params}</p> <p>Task params: #{params[:task]}</p>"
+  end
+end
